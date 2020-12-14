fd178b851342ca8d21d666692c0abf411cb2cd79
Patrick Rocha <ptkdev@outlook.com>
2020-12-14 03:21:08 +0000

    Segregating for diferent archives.

--- StructureOfData.rb
+++ StructureOfData.rb
@@ -9,6 +9,6 @@ loop do
   emptyArray.push(count)
   break if count == 5
   puts 'using push method'
-  puts emptyArray
+  puts "in this moment, the variable emptyArray got: #{emptyArray}"
   count += 1
 end
